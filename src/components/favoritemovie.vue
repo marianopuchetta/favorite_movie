e69a3e11ed8fe370cee5a71dc8e9e4100364a950
masterweed <marianopuchetta@hotmail.com>
2019-10-02 20:22:41 -0300

    add google adsense

--- src/components/favoritemovie.vue
+++ src/components/favoritemovie.vue
@@ -43,6 +43,13 @@
 </template>
 
 <script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-7781491819210330",
+          enable_page_level_ads: true
+     });
+</script>
 import { AXIOS } from './http-common'
 
 export default {
