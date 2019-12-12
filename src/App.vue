30f6e8037d8eace19f023d0449b821f7acf51472
masterweed <marianopuchetta@hotmail.com>
2019-12-12 19:17:34 -0300

    adsense

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <Home/>
+    <Home />
+    <Adsense adSlot = '3791641209'></Adsense>
   </div>
 </template>
 
 <script>
 import Home from './components/favoritemovie.vue'
+import Adsense from './components/Adsense.vue'
 export default {
   name: 'App',
   components: {
+    Adsense,
     Home
   }
 }
 </script>
 
 <style>
-
 </style>
---
+++ src/components/Adsense.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <ins class='adsbygoogle' :style="adStyle" :data-ad-client="adClient" :data-ad-slot="adSlot"><h1>hekii</h1></ins>
+</template>
+<script>
+export default {
+  props: {
+    adStyle: {
+      type: String,
+      required: false,
+      default: 'display:block'
+    },
+    adClient: {
+      type: String,
+      required: false,
+      default: 'ca-pub-7781491819210330'
+    },
+    adSlot: {
+      type: String,
+      required: false,
+      default: '3791641209'
+    }
+  },
+  mounted () {
+    (adsbygoogle = window.adsbygoogle || []).push({})
+  }
+}
+// eslint-disable-next-line
+</script>
