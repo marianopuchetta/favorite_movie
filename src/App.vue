d878266f5dee70ed98a9d40060bf27952d5736c7
masterweed <marianopuchetta@hotmail.com>
2019-12-16 14:44:45 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -19,18 +19,27 @@
 <router-view/> 
  
  <footer>
-   <div>
+   <div id="footer_items">
+      <router-link to="/terms">
+        <h3 id="terms">terms of use</h3></router-link>
+    </div>
+     <div id="footer_item">
+      <router-link to="/terms">
+        <h3 id="terms">terms of u</h3></router-link>
+    </div>
+    <div id="footer_ite">
+      <router-link to="/terms">
+        <h3 id="terms">terms of </h3></router-link>
+    </div>
+   <div id="counter">
    <!-- Contador de visitas --> 
-<center><a title="Contador De Visitas Gratis">
-<img alt="contador de visitas" src="http://www.websmultimedia.com/contador-de-visitas.php?id=275729"></a>
-</center>
+    <center><a title="Contador De Visitas Gratis">
+     <img alt="contador de visitas" src="http://www.websmultimedia.com/contador-de-visitas.php?id=275729"></a>
+    </center>
 <!-- Fin Contador de visitas --> 
-      </div>
-      <div>
-   <router-link to="/terms">
-        <h3 id="footer_items">terms of use</h3></router-link>
-      </div>
-      </footer>
+    </div>
+    
+ </footer>
   </div>
 </template>
 
@@ -47,12 +56,9 @@ export default {
 </script>
 
 <style>
-center {
+#counter {
  border: 0px solid;
  display: flex;
- height: 100px;
- width: 100px;
- bottom:0px;
  }
 #navbar{
  font-family: "Lilita One", cursive;
@@ -67,19 +73,23 @@ center {
 footer{
   background: rgb(15, 127, 219);
   text-align: center;
-   position: fixed;
-   bottom: 0;
-   width: 100%;
-   height: 20px;
-    z-index: 1;
-    display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 20px;
+  z-index: 1;
+  display: flex;
+  justify-content: space-between
 }
-#footer_items{
+footer > #counter{
+  display: flex;
+  justify-items: center;
+}
+#terms{
   font-family: "Lilita One";
-   font-size: 15px;
-color:black;
-   text-shadow: 1px 0 0 #fff, -2px 0 0 #fff, 0 2px 0 #fff, 0 -2px 0 #fff, 1px 1px #fff, -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff;
-
+  font-size: 15px;
+  color:black;
+  text-shadow: 1px 0 0 #fff, -2px 0 0 #fff, 0 2px 0 #fff, 0 -2px 0 #fff, 1px 1px #fff, -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff;
 }
 h4{
   color:black;
