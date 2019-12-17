f2421d1ffc14f410d18119c2dff22831a679cbdd
masterweed <marianopuchetta@hotmail.com>
2019-12-17 18:25:17 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,41 @@
   <div id="app">
     <br><br>
     <div>
-    <nav id="navbar" class="navbar navbar-expand-md  bg-primary navbar-dark fixed-top">
-    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#myNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="https://searchactors.herokuapp.com/">Busca tus pelis</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+   <div>
+  <b-navbar toggleable="sm" class="navbar navbar-expand-md  bg-primary navbar-dark fixed-top">
+    <b-navbar-brand href="https://searchactors.herokuapp.com/">Busca tus pelis</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+       <!-- <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>-->
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
   <Adsense adSlot = '3791641209'></Adsense>
 </div> 
 <router-view/> 
- 
  <footer>
    <div id="footer_items">
-      <router-link to="/terms">
-        <h3 id="terms">terms of use</h3></router-link>
-    </div>
-   <div id="counter">
-   <!-- Contador de visitas --> 
+     <ul>
+       <li>
+          <router-link to="/terms">
+        terms of use</router-link>
+       </li>
+       <li>
+        <router-link to="/terms">
+        terms of u</router-link>
+       </li>
+       <li>
+          <!-- Contador de visitas --> 
     <center><a title="Contador De Visitas Gratis">
      <img alt="contador de visitas" src="http://www.websmultimedia.com/contador-de-visitas.php?id=275729"></a>
     </center>
 <!-- Fin Contador de visitas --> 
-    </div>
-    
+       </li>
+     </ul> 
+   </div> 
  </footer>
   </div>
   </body>
@@ -45,14 +51,19 @@ export default {
   components: {
     Adsense,
     Home
+  },
+  methods: {
+    toggleNavbar() {
+      this.show = !this.show;
+    }
   }
+
 }
 </script>
 
 <style>
 #counter {
  border: 0px solid;
- display: flex;
  }
 #navbar{
  font-family: "Lilita One", cursive;
@@ -61,33 +72,29 @@ export default {
 }
 body{
   background-image: url(assets/tom.jpg);
- 
   text-align: center;
 }
 footer{
   background: rgb(15, 127, 219);
-  text-align: center;
   position: fixed;
   bottom: 0;
   width: 100%;
   height: 20px;
   z-index: 1;
-  display: flex;
-  justify-content: space-between
 }
-footer > #counter{
-  display: flex;
-  align-items: center;
- }
-#terms{
-  font-family: "Lilita One";
+h4{
+  color:black;
+}
+li{
+  list-style:none;
+   font-family: "Lilita One";
   font-size: 15px;
   color:black;
   text-shadow: 1px 0 0 #fff, -2px 0 0 #fff, 0 2px 0 #fff, 0 -2px 0 #fff, 1px 1px #fff, -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff;
   padding:2px;
 }
-h4{
-  color:black;
+#footer_items ul li {
+  display: inline;
 }
 @media only screen and (max-width: 768px){
   
